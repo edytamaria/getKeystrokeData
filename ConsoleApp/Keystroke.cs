cea896db1c0eb1760dbc35bf6065d1b91d2a15de
edytamaria <bandyta.edyta@gmail.com>
2019-05-11 12:42:22 +0200

    initial commit

---
+++ ConsoleApp/Keystroke.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Keystroke
+    {   
+        private 
+
+        public Keystroke(KeyNotFoundException)
+        {
+
+        }
+
+        public void ButtonDown()
+        {
+
+        }
+
+        public void ButtonUp()
+        {
+
+        }
+    }
+}
